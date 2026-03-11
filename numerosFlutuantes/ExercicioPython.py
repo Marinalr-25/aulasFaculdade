f34eb61d0fe20bcd4f6bea7c065eb26277d02efc
Marina <marina.roberto2020@gmail.com>
2026-03-11 18:19:54 -0300

    ajustado formula do ano

--- numerosFlutuantes/ExercicioPython.py
+++ numerosFlutuantes/ExercicioPython.py
@@ -20,6 +20,6 @@ else:
 
 
 if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
-    print(f"{ano} Ano bissexto")
+    print(f"{ano} é ano bissexto")
 else:
-    print(f"{ano} Não é ano bissexto")
+    print(f"{ano} não é ano bissexto")
